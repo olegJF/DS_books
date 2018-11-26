e1e00cfe00faf67e88da1909ce8f663251874541
Oleg Novikov <jf2@ua.fm>
2018-11-26 16:45:19 +0200

    added templates

---
+++ books/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import home
+
+app_name = 'books'
+urlpatterns = [
+    path('', home, name='home'),
+]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+def home(request):
+    qs = Book.objects.all()
+    context = {}
+    context['objects_list'] = qs
+    return render(request, 'books/home.html', context)
+    
