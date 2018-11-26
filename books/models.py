aa370abf4d816e97566ecaf130db6bf1fbe9816e
Oleg Novikov <jf2@ua.fm>
2018-11-26 15:38:15 +0200

    added Book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    book_title = models.CharField(max_length=255, verbose_name='Название книги')
+    authors_info = models.TextField(verbose_name='Информация об авторах')
+    isbn = models.CharField(max_length=20, unique=True,
+                                        verbose_name='ISBN книги')
+    price = models.DecimalField(blank=True, max_digits=10, decimal_places=2, 
+                                                    verbose_name='Цена книги')
+                                                    
+    class Meta:
+        verbose_name = 'Книга'
+        verbose_name_plural = 'Книги'
+        ordering = ['book_title']
+        
+    def __str__(self):
+        return self.name
+        
+        
+    
